aac55779a8dd62e3128325c8e925370b25f7276c
pbpbpb2705 <phambao2705@gmail.com>
2024-08-28 02:45:00 +1000

    1.0.1

--- node_lyra.py
+++ node_lyra.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision.transforms.v2 as T
 import pandas as pd
+import os
 
 class MaskFromColorMultiObject:
     @classmethod
@@ -20,13 +21,14 @@ class MaskFromColorMultiObject:
     FUNCTION = "execute"
     CATEGORY = "VSIH/mask"
 
-    def execute(self, image, room_object, threshold):
-        filename = f"colordata/data.csv"
-        data = pd.read_csv(filename)
-        mask = torch.zeros_like(image)
+    def execute(self, image, room_object):
+        file_path = os.path.join(os.path.dirname(__file__), 'data.csv')
+        data = pd.read_csv(file_path, index_col=0)
+        mask = None
 
         #Get keywords from object list
         keywords = room_object.split(',')
+        
         for word in keywords:
             filtered_data = data[data['Name'].str.contains(word, case=False, na=False)]
 
@@ -35,36 +37,34 @@ class MaskFromColorMultiObject:
                 continue
 
             rgb = filtered_data['Color_Code (R,G,B)']
+            # Reset index to 0
+            rgb = rgb.reset_index(drop=True)
             # RGB format is (R,G,B) so we need to split the string and convert to int
             rgb = rgb.str.split(',', expand=True)
+            # Remove brackets '(' and '(' from the string
+            rgb = rgb.apply(lambda x: x.str.replace('(', ''))
+            rgb = rgb.apply(lambda x: x.str.replace(')', ''))
             red = rgb[0].astype(int)
             green = rgb[1].astype(int)
             blue = rgb[2].astype(int)
             temp = (torch.clamp(image, 0, 1.0) * 255.0).round().to(torch.int)
-            color = torch.tensor([red, green, blue])
-            lower_bound = (color - threshold).clamp(min=0)
-            upper_bound = (color + threshold).clamp(max=255)
+            color = torch.tensor([red[0], green[0], blue[0]])
+            print(color)
+            lower_bound = (color).clamp(min=0)
+            upper_bound = (color).clamp(max=255)
             lower_bound = lower_bound.view(1, 1, 1, 3)
             upper_bound = upper_bound.view(1, 1, 1, 3)
             temp_mask = (temp >= lower_bound) & (temp <= upper_bound)
-            temp_mask = mask.all(dim=-1)
-            temp_mask = mask.float()
-            mask = mask & temp_mask
+            temp_mask = temp_mask.all(dim=-1)
+            temp_mask = temp_mask.float()
+            if mask is None:
+                mask = temp_mask
+            else: 
+                mask = mask + temp_mask
+            #Limit the mask to 1
+            mask = torch.clamp(mask, 0, 1)
 
         return (mask, )
-    
-# Set the web directory, any .js file in that directory will be loaded by the frontend as a frontend extension
-# WEB_DIRECTORY = "./somejs"
-
-
-# # Add custom API routes, using router
-# from aiohttp import web
-# from server import PromptServer
-
-# @PromptServer.instance.routes.get("/hello")
-# async def get_hello(request):
-#     return web.json_response("hello")
-
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
